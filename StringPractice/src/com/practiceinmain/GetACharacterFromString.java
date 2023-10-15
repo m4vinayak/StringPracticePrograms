f6de92db1f9036831410086cb018eb9033c31512
m4vinayak <m4vinayak@gmail.com>
2023-10-15 09:39:46 +0530

    GetACharacterFromString

---
+++ StringPractice/src/com/practiceinmain/GetACharacterFromString.java
@@ -0,0 +1,12 @@
+package com.practiceinmain;
+
+public class GetACharacterFromString {
+	
+	public static void main(String[] args) {
+		String str = "Java is a secure language";
+		//get a character at index 5
+		char ch = str.charAt(5);
+		System.out.println("character at index 5 is: "+ch);
+	}
+
+}
